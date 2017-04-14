ce3806920707d9d34614701e9a49ec732699d327
Ubuntu <ubuntu@jenkins.compute-a423912.oraclecloud.internal>
2017-04-14 15:26:37 +0000

    My first minion

---
+++ GitHubAlexandriaBackupMinion.md
@@ -0,0 +1,9 @@
+# First:
+I talk to Slack, so grabe me a token key and put it in /etc/environment, like this:  
+
+MINIONS_SLACK_WEBHOOK=https://hooks.slack.com/services/XXXXXXXX/BBBBBBBBBB/RanDoMChar4ct3rs
+
+
+# Put me in your crontab
+
+0 3 * * * /home/michaeldouglas/minions/GitHubAlexandriaBackupMinion.sh
---
+++ GitHubAlexandriaBackupMinion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /etc/environment
+
+DOW=$(date +%A)
+
+rm -rf  /backups/old/alexandria_${DOW} && echo "Removing old backup" || echo "No old backup to remove"
+
+mv /backups/alexandria_${DOW} /backups/old/ && echo "Moving the old backup to old folder" || echo "No old backup found to move to old folder" 
+
+git clone --mirror git@github.com:adjetiva/alexandria.git /backups/alexandria_${DOW} && \
+
+curl -X POST --data-urlencode 'payload={"channel": "#desenvolvimento", "username": "GitHubBackupMinion", "text": "Just made a backup of alexandria repository in the Jenkins Machine.", "icon_emoji": ":wink:"}' ${MINIONS_SLACK_WEBHOOK} || \
+
+curl -X POST --data-urlencode 'payload={"channel": "#alertas", "username": "GitHubBackupMinion", "text": "I FAILED TO BACKUP THE Alexandia GitHub Repository from GitHub Today. Take a look in the Jenkins machine.", "icon_emoji": ":shit:"}' ${MINIONS_SLACK_WEBHOOK}
